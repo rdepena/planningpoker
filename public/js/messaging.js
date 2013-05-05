0bed1629d63aa4d272690eacca00110eb11b4b2c
Ricardo De Pena <ricardo.depena@gmail.com>
2013-05-05 09:56:01 -0400

    Implemented pub nub keep alive message to prevent drops during long votes.

--- public/js/messaging.js
+++ public/js/messaging.js
@@ -12,7 +12,6 @@ angular.module('pubnub', [])
 	}
 
 	Messaging.subscribe = function (options) {
-		console.log(options);
 		channel = options.channel;
 		pubnub.subscribe({
 			channel : options.channel,
@@ -22,6 +21,17 @@ angular.module('pubnub', [])
 			reconnect : options.reconnect,
 			presence : options.presence
 		});
+
+		var minutes = 60;
+		//keep alive hack : will remove this once I re-write this using socket IO or comparable solution.
+		var keepAlive = function () {
+			if(minutes > 0) {
+				Messaging.publish("ka");
+				minutes--;
+				setTimeout(keepAlive, 60000);
+			}
+		}
+		setTimeout(keepAlive, 60000);
 	};	
 
 	return Messaging;
