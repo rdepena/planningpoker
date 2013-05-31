52166dfa25589f8413ba19f1521a587f8b015fa3
Ricardo De Pena <ricardo.depena@gmail.com>
2013-05-31 17:44:22 -0400

    added namespaces and improved file organization

---
+++ public/js/directives.js
@@ -0,0 +1,83 @@
+(function (planningShark) {
+
+	//not sure if this is the only place I will declare directives so don't overwrite them.
+	planningShark.directives = planningShark.directives || {};
+	planningShark.directives.simpleTimer = function () {
+		return {
+			restrict : 'A', 
+			transclude : false ,
+			template : 
+				'<p>' +
+	                'timer' +
+	                '<button ng-click="addMinutes()" class="btn btn-small">+1</button>' +
+	                '<strong>{{timeFormat(time.minutes)}}:{{timeFormat(time.seconds)}}</strong>' +
+	                '<button ng-click="startTimer()" class="btn btn-small btn-primary">start</button>' +
+	                '<button ng-click="resetTimer()" class="btn btn-small btn-inverse">reset</button>' + 
+	             '</p>',
+	        replace : true,
+			controller : function ($scope) {
+				self = this;
+				self.running = false;
+				self.seconds = 0;
+				self.stopTimer = false;
+
+				//model object of time.
+				$scope.time = {
+					minutes : 1,
+					seconds : 0
+				};
+
+				//private functions:
+				var tick = function () {
+					self.running = true;
+					if(self.seconds === 0) {
+						alert('ding');
+						self.running = false;
+						return;
+					}
+					if (self.stopTimer) {
+						self.running = false;
+						return;
+					}
+					self.seconds--;
+					var update = function () {
+						$scope.time.minutes = Math.floor(self.seconds / 60);
+						$scope.time.seconds = self.seconds % 60;
+					}
+					 var phase = $scope.$root.$$phase;
+					  if(phase == '$apply' || phase == '$digest') {
+					      update();
+					  } else {
+					    $scope.$apply(update);
+					  }
+					setTimeout(tick, 1000);
+				};
+
+				$scope.startTimer = function () {
+					if (!self.running) {
+						var timeInSeconds = $scope.time.minutes  * 60;
+						self.seconds = timeInSeconds;
+						self.stopTimer = false;
+						tick();
+					}
+				};
+
+				$scope.timeFormat = function (t) {
+					return t < 9 ? "0" + t : t;
+				};
+
+				//Adds one minute to the timer.
+				$scope.addMinutes = function() {
+					$scope.time.minutes++;
+					self.seconds += 60;
+				};
+
+				$scope.resetTimer = function () {
+					self.stopTimer = true;
+					$scope.time.minutes = 1;
+					$scope.time.seconds = 0;
+				};
+			}
+		}
+	}
+})(this.planningShark = this.planningShark || {});
--- public/js/messaging.js
+++ public/js/messaging.js
@@ -1,36 +1,41 @@
-angular.module('pubnub', [])
-.factory('Messaging', function($http) {
+(function (planningShark) {
+	//pubnub is our pubnub messaging implementation, this could be replaced with something else at some point.
+	planningShark.pubnub = angular.module('pubnub', []);
+	
+	//we turn the module into a service.
+	planningShark.pubnub.factory('Messaging', function($http) {
 
-	var Messaging = {};
-	var channel = '';
-	var pubnub = PUBNUB.init({
-		subscribe_key :  'sub'
-	});
-
-	Messaging.publish = function (msg) {
-		$http.post('/publish', { channel : channel, message : msg})
-	}
-
-	Messaging.subscribe = function (options) {
-		channel = options.channel;
-		pubnub.subscribe({
-			channel : options.channel,
-			message : options.message,
-			connect : options.connect,
-			disconnect : options.disconnect,
-			reconnect : options.reconnect,
-			presence : options.presence
+		var Messaging = {};
+		var channel = '';
+		var pubnub = PUBNUB.init({
+			subscribe_key :  'sub'
 		});
 
-		//keep alive hack : will remove this once I re-write this using socket IO or comparable solution.
-		var keepAlive = function () {
-			Messaging.publish("k");
-			setTimeout(keepAlive, 60000);
+		Messaging.publish = function (msg) {
+			$http.post('/publish', { channel : channel, message : msg})
 		}
-		if(options.keepAlive) {
-			setTimeout(keepAlive, 60000);
-		}	
-	};	
 
-	return Messaging;
-});
+		Messaging.subscribe = function (options) {
+			channel = options.channel;
+			pubnub.subscribe({
+				channel : options.channel,
+				message : options.message,
+				connect : options.connect,
+				disconnect : options.disconnect,
+				reconnect : options.reconnect,
+				presence : options.presence
+			});
+
+			//keep alive hack : will remove this once I re-write this using socket IO or comparable solution.
+			var keepAlive = function () {
+				Messaging.publish("k");
+				setTimeout(keepAlive, 60000);
+			}
+			if(options.keepAlive) {
+				setTimeout(keepAlive, 60000);
+			}	
+		};	
+
+		return Messaging;
+	});
+})(this.planningShark = this.planningShark || {});
