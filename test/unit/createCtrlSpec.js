6d21a6aab3d5fb0facf95c5ae33f07b6e89e07f8
Ricardo De Pena <ricardo.depena@gmail.com>
2013-08-06 17:10:06 -0400

    new location for unit tests.

---
+++ test/unit/createCtrlSpec.js
@@ -0,0 +1,63 @@
+/*jslint indent: 4, maxerr: 50, vars: true, nomen: true*/
+/*global jasmine, describe, beforeEach, it, inject, planningShark, expect, rtms, angular, spyOn*/
+
+"use strict";
+
+var $;
+
+describe('controller spec', function () {
+	var location;
+	beforeEach(function () {
+		//set up the module.
+		module("planningShark.services", "planningShark");
+		//we will use this user over and over again.
+		location = jasmine.createSpyObj("location", ["path"]);
+		$ = jasmine.createSpyObj('$', ["cookie", 'removeCookie']);
+	});
+
+	describe('createCtrl function', function () {
+		it("should change the location path upon joining", inject(function ($rootScope, $controller) {
+			//wire up
+			var scope = $rootScope.$new();
+			var controller = $controller(planningShark.poker.createCtrl, {
+				$scope : scope,
+				$location : location
+			});
+			scope.userName = "testName";
+			scope.join();
+
+			//expects
+			expect(location.path).toHaveBeenCalled();
+		}));
+
+		it('should declare open Sessions upon creation', inject(function ($rootScope, $controller) {
+			//wire up
+			var scope = $rootScope.$new();
+			var controller = $controller(planningShark.poker.createCtrl, {
+				$scope : scope,
+				$location : location
+			});
+
+			//expects
+			expect(scope.openSessions).not.toBeUndefined();
+		}));
+
+		it('should delete should remove a cookie', inject(function ($rootScope, $controller) {
+			//wire up
+			var scope = $rootScope.$new();
+			var controller = $controller(planningShark.poker.createCtrl, {
+				$scope : scope,
+				$location : location
+			});
+			var session = {
+				name : "testSessionName"
+			};
+			scope.delete(session);
+
+			//expects
+			expect($.removeCookie).toHaveBeenCalledWith(session.name);
+
+		}));
+
+	});
+});
---
+++ test/unit/socketSpec.js
@@ -0,0 +1,56 @@
+/*jslint indent: 4, maxerr: 50, vars: true, nomen: true*/
+/*global jasmine, describe, beforeEach, it, inject, expect, rtms, angular, spyOn*/
+
+'use strict';
+// Jasmine spec for the socket service.
+var io;
+describe('socket spec', function () {
+	var rtms;
+	beforeEach(function () {
+		//set up the module.
+		module("planningShark.services", "planningShark");
+		io = jasmine.createSpyObj('io', ['connect']);
+		rtms = jasmine.createSpyObj('rtms', ['on', 'emit']);
+		io.connect.andCallFake(function (){
+			return rtms;
+		});
+	});
+	describe('socket service', function () {
+		describe("subcribe function ", function () {
+			it("should emit a room on subscibe", inject(function (socket) {
+				//wire up
+				var roomName = 'testRoom';
+				var options = { roomName : roomName };
+				socket.subscribe(options);
+
+				//expects
+				expect(rtms.emit).toHaveBeenCalledWith("joinRoom", {room : roomName});
+			}));
+
+			it("should subbscribe with the on event", inject(function (socket) {
+				//wire up
+				var roomName = 'testRoom';
+				var options = { roomName : roomName };
+				socket.subscribe(options);
+
+				//expects
+				expect(rtms.on).toHaveBeenCalled();
+
+			}));
+		});
+
+		describe("publish function", function () {
+			it("should emit a broadcast", inject(function (socket) {
+				//wire up
+				var msg = "something",
+					roomName = 'testRoom';
+				socket.subscribe({roomName : roomName});
+				socket.publish(msg);
+
+				//expects
+				expect(rtms.emit).toHaveBeenCalledWith('broadcast', {room : roomName, message : msg});
+
+			}));
+		});
+	});
+});
